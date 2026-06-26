d1c78b0b50d2cde09e169f0ee4bcacbf62b6bce7
AzureAD\早瀬優希 <yuuki.hayase@mcomp.co.jp>
2026-06-26 09:56:23 +0900

    main-commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-    <h1>main02</h1>
+    <h1>mainのブランチです。現状、コミット</h1>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
